d99078f795b309def8430cc50a1480b2f9095d9e
SushiWaUmai <eugene.bb1@gmail.com>
2020-12-02 20:08:49 +0100

    add stuff

---
+++ Runtime/CameraShake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using Basics;
+
+namespace CameraShaker
+{
+    public static class CameraShake
+    {
+        public static void ShakeCamera(this CinemachineVirtualCamera cvc, float time, float amplitude = 1, float frequency = 1)
+        {
+            CinemachineBasicMultiChannelPerlin perlin = cvc.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            perlin.m_AmplitudeGain = amplitude;
+            perlin.m_FrequencyGain = frequency;
+
+            BasicTimeManager.Instance.Wait(time, () =>
+            {
+                perlin.m_AmplitudeGain = 0;
+                perlin.m_FrequencyGain = 0;
+            });
+        }
+
+        public static void ShakeCamera(this CinemachineVirtualCamera cvc, float time, float intensity) => cvc.ShakeCamera(time, intensity, intensity);
+    }
+}
